ce528525cb3c8fa806daabeca137090ae4ee0f3c
pt_888 <pt_888@aol.com>
2024-05-13 13:53:44 -0400

    edit command

--- modules/help/help.py
+++ modules/help/help.py
@@ -8,8 +8,13 @@ class HelpModule(Module):
     def __init__(self, irc):
         super().__init__(irc, "!", "help")
 
-    def handleCommand(self, message, command):
-        if message.command == self.fantasy + self.command:            
-            self.irc.privmsg(message.trailing, "Hello, %s, these are available commands; !cake <nick> !drink <nick> !maid <nick> !ramen <nick> !snack <nick> !tea <nick>" % message.messageFrom)
-            self.irc.privmsg(message.trailing, "To play pigeon game; !shoot and to check score; !score")
+    def handleCommand(self, message, messegeTo):
+        if command.command == "PRIVMSG":
+           if command.command == self.fantasy + self.command:
 
+                    self.irc.privmsg(message.messegeTo, "Hello, %s, these are available commands; !cake <nick> !drink <nick> !maid <nick> !ramen <nick> !snack <nick> !tea <nick>" % message.messageFrom)
+                    self.irc.privmsg(message.messegeTo, "To play pigeon game; !shoot and to check score; !score")
+                    return
+   
+                  
+                         
