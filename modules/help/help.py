da601582b997078d7d79842affbfaa1f9ab4a793
James Trotter <james.trotter@tempmee.com>
2024-05-13 13:57:37 -0400

    fix: variables

--- modules/help/help.py
+++ modules/help/help.py
@@ -9,12 +9,8 @@ class HelpModule(Module):
         super().__init__(irc, "!", "help")
 
     def handleCommand(self, message, command):
-        if command.command == "PRIVMSG":
-           if command.command == self.fantasy + self.command:
-
-                    self.irc.privmsg(message.messegeTo, "Hello, %s, these are available commands; !cake <nick> !drink <nick> !maid <nick> !ramen <nick> !snack <nick> !tea <nick>" % message.messageFrom)
-                    self.irc.privmsg(message.messegeTo, "To play pigeon game; !shoot and to check score; !score")
-                    return
-   
-                  
-                         
+        if message.command == "PRIVMSG":
+            if command.command == self.fantasy + self.command:
+                self.irc.privmsg(message.messegeTo, "Hello, %s, these are available commands; !cake <nick> !drink <nick> !maid <nick> !ramen <nick> !snack <nick> !tea <nick>" % message.messageFrom)
+                self.irc.privmsg(message.messegeTo, "To play pigeon game; !shoot and to check score; !score")
+                return
