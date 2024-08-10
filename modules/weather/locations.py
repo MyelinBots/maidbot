61db5227957421a5272c60792f9cc0a323aabeb2
James <admin@floretos.com>
2024-08-10 11:26:56 -0400

    fix: should all be lowercase for location

--- modules/weather/locations.py
+++ modules/weather/locations.py
@@ -4,26 +4,37 @@ class Locations:
         self.locations = {}
 
     def add_location(self, nick, channel, location):
+        nick = nick.lower()
+        channel = channel.lower()
         print("Adding location", nick, channel, location)
         if channel not in self.locations:
             self.locations[channel] = {}
-        print("contiuining")
         self.locations[channel][nick] = location
 
     def get_location(self, nick, channel):
+        nick = nick.lower()
+        channel = channel.lower()
         print(nick, self.locations)
         if channel in self.locations:
             return self.locations[channel].get(nick, None)
         return None
 
     def remove_location(self, nick, channel):
+        nick = nick.lower()
+        channel = channel.lower()
         if channel in self.locations:
             self.locations[channel].pop(nick, None)
 
     def has_location(self, nick, channel) -> bool:
         # check if nick exists in locations
+        nick = nick.lower()
+        channel = channel.lower()
         if channel in self.locations:
-            return nick in self.locations[channel]
+            for key in self.locations[channel]:
+                if key == nick:
+                    print("Found location", nick, channel)
+                    return True
+        print("Location not found", nick, channel)
         return False
 
     def get_all_locations(self):
